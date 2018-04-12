53e1b23abd50d1769d05a0de914f77a42d2944de
luisarmandodv <luisarmandodv@gmail.com>
2018-04-12 11:21:25 -0500

    Showing table and instructions

---
+++ index.rb
@@ -0,0 +1,21 @@
+table = {
+  1 => " 1 ",
+  2 => " 2 ",
+  3 => " 3 ",
+  4 => " 4 ",
+  5 => " 5 ",
+  6 => " 6 ",
+  7 => " 7 ",
+  8 => " 8 ",
+  9 => " 9 ",
+}
+
+puts "Welcome to my Tic Tac Toe game!"
+puts "Here is a map of my tic tac toe game, just type the square number to start playing! :D"
+
+puts "#{table[1]}|#{table[2]}|#{table[3]}"
+puts "---|---|---"
+puts "#{table[4]}|#{table[5]}|#{table[6]}"
+puts "---|---|---"
+puts "#{table[7]}|#{table[8]}|#{table[9]}"
+puts "---|---|---"
